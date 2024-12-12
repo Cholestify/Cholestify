b7fb437ddc39a0b7d16ffbf0827c6c75fffaf7c5
Fadil0107 <fadilh0107@gmail.com>
2024-12-12 14:34:55 +0700

    membuat perilaku agar ketika sudah login, langsung keluar app ketika tombol back ditekan

--- app/src/main/java/com/example/cholestifyapp/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/cholestifyapp/ui/home/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
@@ -56,6 +57,14 @@ class HomeFragment : Fragment() {
         fetchFoodRecommendations()
         fetchMealFoodNutrition()
         fetchMeals()
+
+        // Menangani tombol back
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                // Menutup aplikasi ketika tombol back ditekan
+                requireActivity().finish()
+            }
+        })
 
         return binding.root
     }
